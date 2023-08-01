53328686336fe38f6ed0eec79d26b64d833f9852
MahfuzNazib <nazibmahfuz60@gmail.com>
2023-08-01 17:07:20 +0600

    Complete Starter WinForm Desing and Adding Basic Validation

--- WebDeployEase-v1.1/Starter.Designer.cs
+++ WebDeployEase-v1.1/Starter.Designer.cs
@@ -28,19 +28,248 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Starter));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtApplicationName = new System.Windows.Forms.TextBox();
+            this.btnInstall = new System.Windows.Forms.Button();
+            this.txtFolderPath = new System.Windows.Forms.TextBox();
+            this.btnFolderPath = new System.Windows.Forms.Button();
+            this.txtDatabaseName = new System.Windows.Forms.TextBox();
+            this.txtDatabaseUserName = new System.Windows.Forms.TextBox();
+            this.txtDatabasePassword = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(42)))), ((int)(((byte)(64)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(-5, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(834, 51);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Siyam Rupali", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(320, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(243, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Web Deploy Ease Installer";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WebDeployEase_v1._1.Properties.Resources._826073;
+            this.pictureBox1.Location = new System.Drawing.Point(270, 7);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(54, 39);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Siyam Rupali", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(150, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Application Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Siyam Rupali", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(150, 181);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Folder Path";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Siyam Rupali", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(150, 217);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 24);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Database Name";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Siyam Rupali", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(150, 252);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(108, 24);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "DB User Name";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Siyam Rupali", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(150, 288);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 24);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Password";
+            // 
+            // txtApplicationName
+            // 
+            this.txtApplicationName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(77)))), ((int)(((byte)(89)))));
+            this.txtApplicationName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtApplicationName.Font = new System.Drawing.Font("Siyam Rupali", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtApplicationName.ForeColor = System.Drawing.Color.White;
+            this.txtApplicationName.Location = new System.Drawing.Point(315, 145);
+            this.txtApplicationName.Margin = new System.Windows.Forms.Padding(0);
+            this.txtApplicationName.Name = "txtApplicationName";
+            this.txtApplicationName.Size = new System.Drawing.Size(331, 24);
+            this.txtApplicationName.TabIndex = 6;
+            // 
+            // btnInstall
+            // 
+            this.btnInstall.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(42)))), ((int)(((byte)(64)))));
+            this.btnInstall.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnInstall.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnInstall.Font = new System.Drawing.Font("Siyam Rupali", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnInstall.ForeColor = System.Drawing.Color.White;
+            this.btnInstall.Location = new System.Drawing.Point(414, 342);
+            this.btnInstall.Name = "btnInstall";
+            this.btnInstall.Size = new System.Drawing.Size(102, 43);
+            this.btnInstall.TabIndex = 7;
+            this.btnInstall.Text = "Install";
+            this.btnInstall.UseVisualStyleBackColor = false;
+            this.btnInstall.Click += new System.EventHandler(this.btnInstall_Click);
+            // 
+            // txtFolderPath
+            // 
+            this.txtFolderPath.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(77)))), ((int)(((byte)(89)))));
+            this.txtFolderPath.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtFolderPath.Font = new System.Drawing.Font("Siyam Rupali", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtFolderPath.ForeColor = System.Drawing.Color.White;
+            this.txtFolderPath.Location = new System.Drawing.Point(315, 181);
+            this.txtFolderPath.Margin = new System.Windows.Forms.Padding(0);
+            this.txtFolderPath.Name = "txtFolderPath";
+            this.txtFolderPath.Size = new System.Drawing.Size(294, 24);
+            this.txtFolderPath.TabIndex = 9;
+            // 
+            // btnFolderPath
+            // 
+            this.btnFolderPath.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(77)))), ((int)(((byte)(89)))));
+            this.btnFolderPath.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFolderPath.Font = new System.Drawing.Font("Siyam Rupali", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnFolderPath.ForeColor = System.Drawing.Color.White;
+            this.btnFolderPath.Location = new System.Drawing.Point(611, 181);
+            this.btnFolderPath.Name = "btnFolderPath";
+            this.btnFolderPath.Size = new System.Drawing.Size(35, 24);
+            this.btnFolderPath.TabIndex = 10;
+            this.btnFolderPath.Text = "...";
+            this.btnFolderPath.UseVisualStyleBackColor = false;
+            this.btnFolderPath.Click += new System.EventHandler(this.btnFolderPath_Click);
+            // 
+            // txtDatabaseName
+            // 
+            this.txtDatabaseName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(77)))), ((int)(((byte)(89)))));
+            this.txtDatabaseName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtDatabaseName.Font = new System.Drawing.Font("Siyam Rupali", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtDatabaseName.ForeColor = System.Drawing.Color.White;
+            this.txtDatabaseName.Location = new System.Drawing.Point(315, 217);
+            this.txtDatabaseName.Margin = new System.Windows.Forms.Padding(0);
+            this.txtDatabaseName.Name = "txtDatabaseName";
+            this.txtDatabaseName.Size = new System.Drawing.Size(331, 24);
+            this.txtDatabaseName.TabIndex = 11;
+            // 
+            // txtDatabaseUserName
+            // 
+            this.txtDatabaseUserName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(77)))), ((int)(((byte)(89)))));
+            this.txtDatabaseUserName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtDatabaseUserName.Font = new System.Drawing.Font("Siyam Rupali", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtDatabaseUserName.ForeColor = System.Drawing.Color.White;
+            this.txtDatabaseUserName.Location = new System.Drawing.Point(315, 252);
+            this.txtDatabaseUserName.Margin = new System.Windows.Forms.Padding(0);
+            this.txtDatabaseUserName.Name = "txtDatabaseUserName";
+            this.txtDatabaseUserName.Size = new System.Drawing.Size(331, 24);
+            this.txtDatabaseUserName.TabIndex = 12;
+            // 
+            // txtDatabasePassword
+            // 
+            this.txtDatabasePassword.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(77)))), ((int)(((byte)(89)))));
+            this.txtDatabasePassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtDatabasePassword.Font = new System.Drawing.Font("Siyam Rupali", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtDatabasePassword.ForeColor = System.Drawing.Color.White;
+            this.txtDatabasePassword.Location = new System.Drawing.Point(315, 288);
+            this.txtDatabasePassword.Margin = new System.Windows.Forms.Padding(0);
+            this.txtDatabasePassword.Name = "txtDatabasePassword";
+            this.txtDatabasePassword.Size = new System.Drawing.Size(331, 24);
+            this.txtDatabasePassword.TabIndex = 13;
             // 
             // Starter
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.ClientSize = new System.Drawing.Size(822, 486);
+            this.Controls.Add(this.txtDatabasePassword);
+            this.Controls.Add(this.txtDatabaseUserName);
+            this.Controls.Add(this.txtDatabaseName);
+            this.Controls.Add(this.btnFolderPath);
+            this.Controls.Add(this.txtFolderPath);
+            this.Controls.Add(this.btnInstall);
+            this.Controls.Add(this.txtApplicationName);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Starter";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Web Deploy Installer";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private TextBox txtApplicationName;
+        private Button btnInstall;
+        private TextBox txtFolderPath;
+        private Button btnFolderPath;
+        private TextBox txtDatabaseName;
+        private TextBox txtDatabaseUserName;
+        private TextBox txtDatabasePassword;
     }
 }
--- WebDeployEase-v1.1/Starter.cs
+++ WebDeployEase-v1.1/Starter.cs
@@ -6,5 +6,62 @@ namespace WebDeployEase_v1._1
         {
             InitializeComponent();
         }
+
+        private void btnFolderPath_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+
+            if(folderBrowserDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK )
+            {
+                txtFolderPath.Text = folderBrowserDialog.SelectedPath;
+            }
+            else
+            {
+                MessageBox.Show("Please Select a Valid Folder Path", "Error",MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnInstall_Click(object sender, EventArgs e)
+        {
+            if (UserInputValidation())
+            {
+                
+            }
+        }
+
+        private bool UserInputValidation()
+        {
+            if(string.IsNullOrEmpty(txtApplicationName.Text))
+            {
+                MessageBox.Show("Please Define Application Name","Invalid",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(txtFolderPath.Text))
+            {
+                MessageBox.Show("Please Define Application Folder Path", "Invalid", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(txtDatabaseName.Text))
+            {
+                MessageBox.Show("Please Define Database Name", "Invalid", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(txtDatabaseUserName.Text))
+            {
+                MessageBox.Show("Please Enter Database User Name", "Invalid", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(txtDatabasePassword.Text))
+            {
+                MessageBox.Show("Please Enter Database Password", "Invalid", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            return true;
+        }
     }
 }
