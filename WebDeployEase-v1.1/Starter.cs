42755298e6156aaf54a4f7579e10aa85d71c45c9
MahfuzNazib <nazibmahfuz60@gmail.com>
2023-08-01 15:24:47 +0600

    Starter Form Page Design

--- WebDeployEase-v1.1/Starter.cs
+++ WebDeployEase-v1.1/Starter.cs
@@ -1,8 +1,8 @@
 namespace WebDeployEase_v1._1
 {
-    public partial class Form1 : Form
+    public partial class Starter : Form
     {
-        public Form1()
+        public Starter()
         {
             InitializeComponent();
         }
